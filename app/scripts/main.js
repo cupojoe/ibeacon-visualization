e0960c2681ce2af2a777683e26742dd0af5b1876
cupojoe <joe.fraga@gmail.com>
2014-06-02 18:45:12 -0400

    Updated the JS structure to use RequireJS

---
+++ app/scripts/main.js
@@ -0,0 +1,29 @@
+/*
+*   main.js
+*   This file is where all scripts will be configured and/or imported
+*/
+
+'use strict';
+require.config({
+    paths: {
+        domReady: '../bower_components/requirejs-domready/domReady',
+        jquery: '../bower_components/jquery/dist/jquery',
+        socketio: '../bower_components/socket.io-client/dist/socket.io',
+        app: 'app'
+    },
+    shim: {
+        socketio: {
+            exports: 'io'
+        },
+        app: {
+            deps: ['jquery']
+        }
+    }
+});
+
+require(['app', 'domReady'], function (app, domReady) {
+    domReady(function () {
+        // use app here
+        app.init();
+    });
+});
